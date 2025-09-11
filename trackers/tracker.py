dc55d581950a58fd09bdb47b18279fa83485228c
ArjunShanmukh <arjun.shanmukh@protonmail.com>
2025-09-10 23:37:24 -0400

    Update tracker.py

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -27,22 +27,6 @@ class Tracker:
                             position = get_foot_position(bbox)
                     tracks[object][frame_num][track_id]['position'] = position
 
-    def interpolate_ball_positions(self, ball_positions):
-        ball_positions = [x.get(1, {}).get('bbox', [0, 0, 0, 0]) for x in ball_positions]
-        df_ball_positions = pd.DataFrame(ball_positions, columns=['x1', 'y1', 'x2', 'y2'])
-
-        # Replace placeholder [0, 0, 0, 0] with NaN for interpolation
-        df_ball_positions = df_ball_positions.replace([0, 0, 0, 0], np.nan)
-
-        # Interpolate missing values
-        df_ball_positions = df_ball_positions.interpolate(method='linear', limit_direction='both')
-        df_ball_positions = df_ball_positions.bfill().ffill()
-
-        # Convert back to list of dictionaries
-        ball_positions = [{1: {"bbox": x}} for x in df_ball_positions.to_numpy().tolist()]
-
-        return ball_positions
-
     def detect_frames(self, frames):
         batch_size = 20
         detections = []
@@ -175,25 +159,6 @@ class Tracker:
 
         return frame
 
-    #def draw_team_ball_control(self, frame, frame_num, team_ball_control):
-        #Draw a semi-transparent rectangle
-        #overlay = frame.copy()
-        #cv2.rectangle(overlay, (1350, 850), (1900, 970), (255,255,255), -1 )
-        #alpha = 0.4
-        #cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
-
-       # team_ball_control_till_frame = team_ball_control[:frame_num+1]
-        # Get the number of times each time had ball control
-        #team_1_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==1].shape[0]
-        #team_2_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==2].shape[0]
-        #team_1 = team_1_num_frames/(team_1_num_frames+team_2_num_frames)
-        #team_2 = team_2_num_frames/(team_1_num_frames+team_2_num_frames)
-
-        #cv2.putText(frame, f{"Team 1 Ball Control:  {team_1*100: 2f}%", (1400,900), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3})
-        #cv2.putText(frame, f{"Team 1 Ball Control:  {team_1*100: 2f}%", (1400,950), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3})
-
-        #return frame
-
     def draw_annotations(self, video_frames, tracks, # team_ball_control
                           ):
         output_video_frames = []
@@ -201,7 +166,6 @@ class Tracker:
             frame = frame.copy()
 
             player_dict = tracks["players"][frame_num]
-            ball_dict = tracks["ball"][frame_num]
             referee_dict = tracks["referees"][frame_num]
 
             # Draw Players
@@ -216,14 +180,9 @@ class Tracker:
             for _, referee in referee_dict.items():
                 frame = self.draw_ellipse(frame, referee["bbox"], (0, 225, 255))
 
-            # Draw Ball
-            for track_id, ball in ball_dict.items():
-                frame = self.draw_triangle(frame, ball["bbox"], (0, 255, 0))
-
-
-            # Draw Team Ball Control
-            #frame = self.draw_team_ball_control(frame,frame_num,team_ball_control)
+        
 
             output_video_frames.append(frame)
+
 
         return output_video_frames
